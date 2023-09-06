dfbea0fed4d88fd95462a9662c20f7a07caec18d
Luz Vazquez <vgluzmaria96@gmail.com>
2023-09-06 09:00:53 -0600

    bootstrap

--- src/layout/login.jsx
+++ src/layout/login.jsx
@@ -1,8 +1,10 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 function LoginLayout() {
   return (
     <div>
       <h1> Login layout</h1>
+      <Button variant="primary">Primary</Button>{' '}
     </div>
   )
 }
