f1b8dbc2d33cc5ec29d2d097852bab6610bbf9f4
Luz Vazquez <vgluzmaria96@gmail.com>
2023-09-05 22:33:47 -0600

    estructura

---
+++ src/layout/login.jsx
@@ -0,0 +1,10 @@
+
+function LoginLayout() {
+  return (
+    <div>
+      <h1> Login layout</h1>
+    </div>
+  )
+}
+
+export default LoginLayout
---
+++ src/pages/usuarios.jsx
@@ -0,0 +1,11 @@
+
+
+const UsuarioPage = () => {
+  return (
+    <div>
+      <h1>Usuarios</h1>
+    </div>
+  )
+}
+
+export default UsuarioPage
