3a81606ab1c72f31ba61d4b9fdb8e03b13d2886d
Luz Vazquez <vgluzmaria96@gmail.com>
2023-09-11 21:59:45 -0600

    Modal de productos

---
+++ src/components/modals/productos.modal.jsx
@@ -0,0 +1,47 @@
+import { Button, Form, FormGroup, Modal } from "react-bootstrap";
+import  PropTypes  from "prop-types"
+
+
+const ProductosModal = ({type}) => {
+    return (
+        <div className="bg-dark text-white">
+            <Modal.Header closeButton>
+                <Modal.Title>{type} Productos</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <Form   >
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control type="text" placeholder="" />
+      </Form.Group>
+      <FormGroup>
+      <Form.Label>Tipo</Form.Label>
+
+      <Form.Select aria-label="Default select example">
+      <option>Tipo </option>
+      <option value="1">Desayuno</option>
+      <option value="2">Resto del dia</option>
+    </Form.Select>
+    </FormGroup>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+      <Form.Label>Categoria</Form.Label>
+        <Form.Control type="text" placeholder="" />
+      </Form.Group>
+    </Form>
+            </Modal.Body>
+            <Modal.Footer>
+            {type === "Editar" ? (
+          <Button variant="success">Editar Producto</Button>
+        ) : (
+          <Button variant="primary">Crear Producto</Button>
+        )}
+        </Modal.Footer>
+
+        </div>
+    );
+}
+ProductosModal.propTypes ={
+    type:PropTypes.string.isRequired
+}
+
+export default ProductosModal
--- src/pages/admin/productos.jsx
+++ src/pages/admin/productos.jsx
@@ -1,70 +1,91 @@
+import { useState } from "react";
 import { Table } from "react-bootstrap"
+import { Modal } from "react-bootstrap";
+import ProductosModal from "../../components/modals/productos.modal";
 
 
 const ProductosPage = () => {
-  return (
-    <div>
-      <br />
-      <div className="container background-table ">
-          <div className="row justify-content-md-end p-3" >
-            <div className="col col-md-3">
-            <button type="button" className="btn btn-success" aria-label="Left Align">
-             <span className="fa fa-user-plus fa-lg" aria-hidden="true"></span> Agregar Producto
-            </button>            
+
+    //declaración de una variable de estado
+
+    const [showModal, setShowModal] = useState(false);
+    const [tipoModal, setTipoModal] = useState('')
+
+    const mostrarModal = (type) => {
+        setShowModal(true)
+        setTipoModal(type)
+    }
+    const ocultarModal = () => {
+        setShowModal(false)
+
+    }
+
+    return (
+        <>
+            <br />
+            <div className="container background-table ">
+                <div className="row justify-content-md-end p-3" >
+                    <div className="col col-md-3">
+                        <button onClick={() => mostrarModal('Registrar')} type="button" className="btn btn-success" aria-label="Left Align">
+                            <span className="fa fa-user-plus fa-lg" aria-hidden="true"></span> Agregar Producto
+                        </button>
+                    </div>
+                </div>
+                <div className="row  p-3">
+                    <div className="col-md-12">
+                        <Table responsive="sm" className='table'>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nombre</th>
+                                    <th>Categoria</th>
+                                    <th>Precio</th>
+                                    <th className='text-end'>Acciones</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Jugo de naranja</td>
+                                    <td>Desayuno</td>
+                                    <td>$32</td>
+                                    <td className='text-end'>
+                                        <button onClick={() => mostrarModal('Editar')}  type="button" className="btn btn-success me-1" aria-label="Left Align">
+                                            <span className="fa fa-user-edit fa-lg" aria-hidden="true"></span> Editar
+                                        </button>
+                                        <button type="button" className="btn btn-danger ms-1" aria-label="Left Align">
+                                            <span className="fa fa-trash fa-lg" aria-hidden="true"></span> Eliminar
+                                        </button>
+                                    </td>
+
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Cafe americano</td>
+                                    <td>Resto del día</td>
+                                    <td>$80</td>
+                                    <td className='text-end'>
+                                        <button type="button" className="btn btn-success me-1" aria-label="Left Align">
+                                            <span className="fa fa-user-edit fa-lg" aria-hidden="true"></span> Editar
+                                        </button>
+                                        <button type="button" className="btn btn-danger ms-1" aria-label="Left Align">
+                                            <span className="fa fa-trash fa-lg" aria-hidden="true"></span> Eliminar
+                                        </button>
+                                    </td>
+
+                                </tr>
+
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="row  p-3">
-            <div className="col-md-12">
-            <Table responsive="sm" className='table'>
-                <thead>
-                 <tr>
-                <th>#</th>
-            <th>Nombre</th>
-            <th>Categoria</th>
-            <th>Precio</th>
-            <th className='text-end'>Acciones</th>
-           
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Jugo de naranja</td>
-            <td>Desayuno</td>
-            <td>$32</td>
-            <td className='text-end'> 
-            <button type="button" className="btn btn-success me-1" aria-label="Left Align">
-             <span className="fa fa-user-edit fa-lg" aria-hidden="true"></span> Editar
-            </button>   
-            <button type="button" className="btn btn-danger ms-1" aria-label="Left Align">
-             <span className="fa fa-trash fa-lg" aria-hidden="true"></span> Eliminar
-            </button>   
-            </td>
-           
-          </tr>
-          <tr>
-          <td>1</td>
-          <td>Cafe americano</td>
-            <td>Resto del día</td>
-            <td>$80</td>
-            <td className='text-end'>
-            <button type="button" className="btn btn-success me-1" aria-label="Left Align">
-             <span className="fa fa-user-edit fa-lg" aria-hidden="true"></span> Editar
-            </button>   
-            <button type="button" className="btn btn-danger ms-1" aria-label="Left Align">
-             <span className="fa fa-trash fa-lg" aria-hidden="true"></span> Eliminar
-            </button>   
-            </td>
-           
-          </tr>
-          
-        </tbody>
-      </Table>
-            </div>
-          </div>
-        </div>
-    </div>
-  )
+            <Modal dialogClassName="custom-modal" show={showModal} onHide={ocultarModal} variant="success">
+                <ProductosModal type={tipoModal}></ProductosModal>
+            </Modal>
+        </>
+    )
 }
 
 export default ProductosPage
