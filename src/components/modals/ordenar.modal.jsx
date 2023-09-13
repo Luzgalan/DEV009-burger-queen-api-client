02ae07c1fdd18f896e7e18800668048bd28e9aad
Katherine Cevallos <katycevallos127@gmail.com>
2023-09-13 02:53:12 -0500

    Se eliminaron los bordel del modal de productos

--- src/components/modals/ordenar.modal.jsx
+++ src/components/modals/ordenar.modal.jsx
@@ -4,24 +4,24 @@ import  PropTypes  from "prop-types"
 const OrdenarModal = ({type}) => {
     return (
         <div className="bg-dark text-white">
-            <Modal.Header closeButton>
+            <Modal.Header closeButton >
                 <Modal.Title>{type} Pedido</Modal.Title>
             </Modal.Header>
             <Modal.Body >
-            <Table striped bordered hover>
+            <Table striped  hover>
       <thead>
         <tr>
           <th>Producto </th>
           <th>Cantidad</th>
           <th >Precio</th>
-          <th >Eliminar</th>
+          <th ></th>
         </tr>
       </thead>
       <tbody>
         <tr>
          
           <td>Sandwich de jamon</td>
-          <td>  <span className="fa fa-circle-plus fa-lg" style={{color:"#33540b",}} aria-hidden="true"></span> 1  <span className="fa fa-circle-minus fa-lg" style={{color:"#33540b",}} aria-hidden="true"></span></td>
+          <td>  <span className="fa fa-circle-plus fa-lg" style={{color:"#33540b",}} aria-hidden="true"></span>   1  <span className="fa fa-circle-minus fa-lg" style={{color:"#33540b",}} aria-hidden="true"></span></td>
           <td>$30,00</td>
           <td><span className="fa fa-trash-can fa-lg" style={{color: "#ffffff",}} aria-hidden="true"></span></td>
         </tr>
