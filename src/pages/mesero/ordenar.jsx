bc985604e013ad45105508ed2ce7a816d7cf7f4a
Katherine Cevallos <katycevallos127@gmail.com>
2023-09-21 01:08:20 -0500

    Navegacion de productos de acuerdo a su tipo sin diseno(en la pagina de ordenar), unicamente funcionalidad

--- src/pages/mesero/ordenar.jsx
+++ src/pages/mesero/ordenar.jsx
@@ -8,17 +8,20 @@ import { getAllProductos } from "../../service/producto";
 
 const OrdenarPages = () => {
     //declaración de una variable de estado
-   
+
     const [showModal, setShowModal] = useState(false);
     const [tipoModal, setTipoModal] = useState('')
 
-    const [productos,setProductos]= useState([])
+    const [productos, setProductos] = useState([])
+
+    //Se crea este estado para el filtro de los productos de acuerdo a su tipo 
+    const [tipoFiltro, setTipoFiltro] = useState('');
 
     //funcion useEfect para hacer la peticion cuando cargue la pagina 
-useEffect(()=>{
-    //consumir servicio 
-    getAllProductos().then(respon=> setProductos(respon));
-   },[])
+    useEffect(() => {
+        //consumir servicio 
+        getAllProductos().then(respon => setProductos(respon));
+    }, [])
 
     const mostrarModal = (type) => {
         setShowModal(true)
@@ -29,158 +32,57 @@ useEffect(()=>{
 
     }
 
+    // Actualizar el estado 'tipoFiltro' con el tipo seleccionado
+    const filtrarProductos = (tipo) => {
+        setTipoFiltro(tipo);
+      };
+
     return (
         <>
-        <div className="container overflow-hidden ">
-            <br />
-            <div className="row mb-5">
-                <div className="col-md-3  ">
-                    <h2 className="text-white">Desayuno</h2>
-                </div>
-                <div className="col-md-3">
-                    <h2 className="text-white">Resto del día</h2>
-                </div>
-                <div className="col-md-3 offset-md-3">
-                    <button onClick={() => mostrarModal('Registrar')} type="button" className="btn btn-success me-1" aria-label="Left Align">
-                        <span className="fa fa-user-edit fa-lg" aria-hidden="true"></span> Ver orden
-                    </button>
-                </div>
+            <div className="container overflow-hidden ">
+                <br />
+                <div className="row mb-5">
+                    <div className="col-md-3  ">
+                        <button className="text-white" onClick={() => filtrarProductos('Desayuno')}>Desayuno</button>
+                    </div>
+                    <div className="col-md-3">
+                        <button className="text-white" onClick={() => filtrarProductos('Almuerzo')}>Resto del día</button>
+                    </div>
+                    <div className="col-md-3">
+                        <button className="text-white" onClick={() => filtrarProductos('Resto del día')} >Resto del día</button>
+                    </div>
+                    <div className="col-md-3 offset-md-3">
+                        <button onClick={() => mostrarModal('Registrar')} type="button" className="btn btn-success me-1" aria-label="Left Align">
+                            <span className="fa fa-user-edit fa-lg" aria-hidden="true"></span> Ver orden
+                        </button>
+                    </div>
 
-            </div>
-            <div className="row g-5">
+                </div>
+                <div className="row g-5">
 
-            {productos.map((producto) => (
-            <div className="col-xl-3 col-md-4 col-sm-6" key={producto.id}>
-              <Card className="fondo-de-tarjeta">
-                <div>
-                  <img className="cardImg" src={producto.image} alt={producto.name} />
-                 
+                    {productos
+                    .filter((producto) => !tipoFiltro || producto.type === tipoFiltro)
+                    .map((producto) => (
+                        <div className="col-xl-3 col-md-4 col-sm-6" key={producto.id}>
+                            <Card className="fondo-de-tarjeta">
+                                <div>
+                                    <img className="cardImg" src={producto.image} alt={producto.name} />
+
+                                </div>
+                                <Card.Body>
+                                    <Card.Title className="texto-tarjeta">{producto.name}</Card.Title>
+                                    <Card.Text className="texto-tarjeta">${producto.price}</Card.Text>
+                                    <Button variant="success">Agregar</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    ))}
                 </div>
-                <Card.Body>
-                  <Card.Title className="texto-tarjeta">{producto.name}</Card.Title>
-                  <Card.Text className="texto-tarjeta">${producto.price}</Card.Text>
-                  <Button variant="success">Agregar</Button>
-                </Card.Body>
-              </Card>
             </div>
-          ))}
-                {/* <div className=" col-xl-3 col-md-4 col-sm-6 ">
-                    <Card className="fondo-de-tarjeta" >
-                        <div>
-                        <img  className="cardImg" src="/src/assets/jugo de naranja.jpeg" alt="Jugo de naranja" />
-                        </div>
-                       
-                        <Card.Body>
-                            <Card.Title>Jugo de naranja</Card.Title>
-                            <Card.Text>
-                                $7
-                            </Card.Text>
-                            <Button variant="success">Agregar</Button>
-                        </Card.Body>
-                    </Card>
-                </div> */}
-                {/* <div className=" col-xl-3 col-md-4 col-sm-6 ">
-                    <Card className="fondo-de-tarjeta">
-                    <div>
-                        <img  className="cardImg" src="/src/assets/sandwi.jpg" alt="Jugo de naranja" />
-                        </div>
-                        <Card.Body>
-                            <Card.Title>Sandwich de jamón y queso</Card.Title>
-                            <Card.Text>
-                $10
-                            </Card.Text>
-                            <Button variant="success">Agregar</Button>
-                        </Card.Body>
-                    </Card> */}
-                {/* </div> <div className=" col-xl-3 col-md-4 col-sm-6 ">
-                    <Card className="fondo-de-tarjeta">
-                    <div>
-                        <img  className="cardImg" src="/src/assets/aros.jpg" alt="Jugo de naranja" />
-                        </div>
-                        <Card.Body>
-                            <Card.Title>Aros de cebolla</Card.Title>
-                            <Card.Text>
-                              $5  
-                            </Card.Text>
-                            <Button variant="success">Agregar</Button>
-                        </Card.Body>
-                    </Card>
-                </div> 
-                <div className=" col-xl-3 col-md-4 col-sm-6 ">
-                    <Card className="fondo-de-tarjeta">
-                    <div>
-                        <img  className="cardImg" src="/src/assets/cafe.jpg" alt="Jugo de naranja" />
-                        </div>
-                        <Card.Body>
-                            <Card.Title>Cafe americano</Card.Title>
-                            <Card.Text>
-                                $5
-                            </Card.Text>
-                            <Button variant="success">Agregar</Button>
-                        </Card.Body>
-                    </Card>
-                </div> */}
-                {/* <div className=" col-xl-3 col-md-4 col-sm-6 ">
-                    <Card className="fondo-de-tarjeta">
-                    <div>
-                        <img  className="cardImg" src="/src/assets/hamburguesa.jpg" alt="Jugo de naranja" />
-                        </div>
-                        <Card.Body>
-                            <Card.Title>Hamburguesa doble</Card.Title>
-                            <Card.Text>
-                                $15
-                            </Card.Text>
-                            <Button variant="success">Agregar</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className=" col-xl-3 col-md-4 col-sm-6 ">
-                    <Card className="fondo-de-tarjeta">
-                    <div>
-                        <img  className="cardImg" src="/src/assets/papasfri.jpg" alt="Jugo de naranja" />
-                        </div>
-                        <Card.Body>
-                            <Card.Title>Papas fritas</Card.Title>
-                            <Card.Text>
-                               $5
-                            </Card.Text>
-                            <Button variant="success">Agregar</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className=" col-xl-3 col-md-4 col-sm-6 ">
-                    <Card className="fondo-de-tarjeta">
-                    <div>
-                        <img  className="cardImg" src="/src/assets/cafeleche.jpg" alt="Jugo de naranja" />
-                        </div>                        <Card.Body>
-                            <Card.Title>Cafe con leche</Card.Title>
-                            <Card.Text>
-                              $10
-                            </Card.Text>
-                            <Button variant="success">Agregar</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className=" col-xl-3 col-md-4 col-sm-6 ">
-                    <Card className="fondo-de-tarjeta">
-                    <div>
-                        <img  className="cardImg" src="/src/assets/jugo de naranja.jpeg" alt="Jugo de naranja" />
-                        </div>
-                        <Card.Body>
-                            <Card.Title>Jugo de pepino</Card.Title>
-                            <Card.Text>
-                                $6
-                            </Card.Text>
-                            <Button variant="success">Agregar</Button>
-                        </Card.Body>
-                    </Card>
-                </div> */}
-            </div>
-        </div>
-         <Modal dialogClassName="custom-modal" show={showModal} onHide={ocultarModal} variant="success">
-         <OrdenarModal type={tipoModal}></OrdenarModal>
-     </Modal>
-      </>
+            <Modal dialogClassName="custom-modal" show={showModal} onHide={ocultarModal} variant="success">
+                <OrdenarModal type={tipoModal}></OrdenarModal>
+            </Modal>
+        </>
     )
 }
 
