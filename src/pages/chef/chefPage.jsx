dc991f6a7a61f7dd52cc85129fbae84cd1d0bafe
Katherine Cevallos <katycevallos127@gmail.com>
2023-09-29 09:42:19 -0500

    Funcionamiento del botón orden lista cambio de status de pending a delivering

--- src/pages/chef/chefPage.jsx
+++ src/pages/chef/chefPage.jsx
@@ -1,40 +1,73 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { getAllOrdenes } from "../../service/orders"
+import { getAllOrdenes, updateOrden } from "../../service/orders"
 import { useEffect, useState } from "react"
 
 const ChefPage = () => {
 
-    const [ordenes, setOrdenes] = useState([]) //Se crea este estado para mostrar las ordenes del la api
+    
+    const [ordenesPendientes, setOrdenesPendientes] = useState([]); // Estado para órdenes pendientes
+    const [ordenesEntregando, setOrdenesEntregando] = useState([]); 
 
     useEffect(() => {
-        //consumir servicio 
+        // Consumir servicio
         getAllOrdenes().then(respon => {
-            const filterResp = respon.filter((orden) => orden.status === "pending")
-            console.log(respon)
-            setOrdenes(filterResp)
-        })
-    }, [])
+            const filterRespPendientes = respon.filter((orden) => orden.status === "pending");
+            setOrdenesPendientes(filterRespPendientes);
+        });
+    }, []);
 
     const convertirMarcaTiempo = (marcaTiempo) => {
-        const fechaHora = new Date(marcaTiempo);
-        const options = {
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric'
-        };
-        return fechaHora.toLocaleDateString('es-ES', options);
+        if (typeof marcaTiempo === 'number') {
+            // Si la marca de tiempo es un número, asumimos que está en milisegundos.
+            const fechaHora = new Date(marcaTiempo);
+            const options = {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric'
+            };
+            return fechaHora.toLocaleDateString('es-ES', options);
+        } else if (typeof marcaTiempo === 'string') {
+            console.log(marcaTiempo);
+            // Si la marca de tiempo es una cadena, asumimos que ya está en el formato deseado.
+            return marcaTiempo;
+            
+        } else {
+            // En caso de que no sea ni un número ni una cadena válida, puedes manejarlo según tus necesidades.
+            return 'Formato de fecha no válido';
+        }
+        
+    };     
+      
+
+    const moverOrdenEntregando = async (orden) => {
+        try {
+            // Cambia el estado de la orden a "delivering" en la API
+            console.log("ID de la orden antes de la eliminación:", orden.id);
+
+            const updatedOrden = await updateOrden(orden.id, { status: "delivering" });
+
+            // Actualiza las listas de órdenes pendientes y entregando en el estado local
+            setOrdenesEntregando([...ordenesEntregando, updatedOrden]);
+            setOrdenesPendientes(ordenesPendientes.filter((o) => o.id !== orden.id));
+            console.log(ordenesPendientes);
+        } catch (error) {
+            console.error('Error al actualizar la orden:', error);
+            
+            // Maneja el error aquí (por ejemplo, muestra un mensaje al usuario)
+        }
     };
 
     return (
         <div className='chef-container'>
             <h1>Bienvenido Chef</h1>
             <div className='orders-container'>
-                <div className=''>
-                    <h2>Pedidos Pendientes</h2>
-                    {ordenes.map((orden) => (
+                <h2>Pedidos Pendientes</h2>
+                <div className='container-cards-orders-pending'>
+
+                    {ordenesPendientes.map((orden) => (
                         <Card key={orden.id} style={{ width: '18rem' }} className='orders-cards-back'>
                             <Card.Body>
                                 <Card.Title>Nombre: {orden.client}</Card.Title>
@@ -45,30 +78,35 @@ const ChefPage = () => {
                                     {orden.products.map((producto, index) => (
                                         <div key={index}>
                                             <p>{producto.cantidad} {producto.name}</p>
-                                            {/* <p>{producto.name}</p> */}
+                                            
                                         </div>
                                     ))}
                                 </Card.Text>
-                                <Button variant="warning">Orden lista</Button>
+                                <Button variant="warning" onClick={() => moverOrdenEntregando(orden)}>Orden lista</Button>
                             </Card.Body>
                         </Card>
                     ))}
                 </div>
-                <div>
-                    <h2>Pedidos Entregados</h2>
-                    <Card style={{ width: '18rem' }} className='orders-cards-delivered'>
-                        <Card.Body>
-                            <Card.Title>Nombre: </Card.Title>
-                            <Card.Title>Fecha: </Card.Title>
-                            <Card.Title>Hora: </Card.Title>
-                            <hr></hr>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the cards content.
-                            </Card.Text>
-
-                        </Card.Body>
-                    </Card>
+                <h2>Pedidos Entregados</h2>
+                <div className='container-cards-orders-delivering'>
+                    {ordenesEntregando.map((orden) => (
+                        <Card key={orden.id} style={{ width: '18rem' }} className='orders-cards-delivered'>
+                            <Card.Body>
+                                <Card.Title>Nombre: {orden.client}</Card.Title>
+                                <Card.Title>Mesa: {orden.mesa}</Card.Title>
+                                <Card.Title>Fecha y Hora: {convertirMarcaTiempo(orden.dateEntry)}</Card.Title>
+                                
+                                <hr />
+                                <Card.Text>
+                                    {orden.products.map((producto, index) => (
+                                        <div key={index}>
+                                            <p>{producto.cantidad} {producto.name}</p>
+                                        </div>
+                                    ))}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ))}
                 </div>
             </div>
         </div>
--- src/service/orders.js
+++ src/service/orders.js
@@ -37,3 +37,31 @@ export const getAllOrdenes = async () => {
   }
 
 }
+
+export const updateOrden = async (orderId, updatedData) => {
+  try {
+    const response = await fetch(`${baseUrl}/orders/${orderId}`, {
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedData),
+    });
+
+    if (response.status === 200) {
+      return await response.json();
+    } else if (response.status === 401) {
+      // Maneja el caso de token no válido aquí
+      throw new Error('Token inválido, inicie sesión nuevamente');
+    } else if (response.status === 404) {
+      throw new Error('Orden no encontrada');
+    } else {
+      // Maneja otros códigos de estado aquí
+      throw new Error('Error al actualizar la orden');
+    }
+  } catch (error) {
+    console.error('Error al actualizar la orden:', error);
+    throw error;
+  }
+};
